ca3e5f9fa0dc5d13da9cbfa8bc151e1e21e861b6
Pasadorr <168021670+Pasadorr@users.noreply.github.com>
2024-12-18 20:29:56 +0300

    Merge remote-tracking branch 'origin/main'

--- pythonProject1/Module 1/Homework2/Homework2.py
+++
@@ -1,8 +0,0 @@
-name = 'Сабржан'
-print(name)
-age = 28
-print(age)
-new_age = age + 1
-print(new_age)
-is_student = True
-print (is_student)
--- pythonProject1/Module 1/Homework3/probe.py
+++
@@ -1,5 +0,0 @@
-user = input('введите координаты цели: ')
-print(type(user))
-x, y = map(int, input(user .split()))
-
-
